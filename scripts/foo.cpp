5738041fc4dc5c5a0ec18918309e19080cddf302
Benjamin Sergeant <bsergean@gmail.com>
2020-11-04 16:28:13 -0800

    mock code

--- scripts/foo.cpp
+++ scripts/foo.cpp
@@ -1,8 +1,28 @@
-#define AL_ENABLED(a, b) if (false)
+
+class FeatureSettings
+{
+public:
+    FeatureSettings() = default;
+    bool __isFeatureEnabled(const char* feature) const { return false; }
+};
+
+class Context
+{
+public:
+    Context() = default;
+    const FeatureSettings& featureSettings() const { return _featureSettings; }
+private:
+    FeatureSettings _featureSettings;
+};
+
+#define AL_ENABLED( name, context ) \
+    if (context.featureSettings().__isFeatureEnabled((name )))
 
 int main()
 {
-    AL_ENABLED("bar_feature", "context")
+    Context context;
+
+    AL_ENABLED("bar_feature", context)
     {
         return 0;
     }
